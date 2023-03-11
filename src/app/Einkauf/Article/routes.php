6816b3c1e1286eeab68bcc8cdfd046ef210e8be2
Sattelpunkt <keven.sattelpunkt@mail.de>
2023-03-11 18:45:31 +0100

    Artikel können angelegt werden

--- src/app/Einkauf/Article/routes.php
+++ src/app/Einkauf/Article/routes.php
@@ -1,3 +1,7 @@
 <?php
 
 $this->add('/', 'Einkauf\Article\Controller\Home', 'GET');
+
+$this->add('/article/new', 'Einkauf\Article\Controller\New', 'GET');
+
+$this->add('/article/new', 'Einkauf\Article\Controller\New@insert', 'POST');
