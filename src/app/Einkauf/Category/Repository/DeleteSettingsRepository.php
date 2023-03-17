29d51512bacfa7dc871b22fe5cee6cd5b5bfdfd4
Sattelpunkt <keven.sattelpunkt@mail.de>
2023-03-17 21:17:20 +0100

    Bei der Löschung eines Shop oder einer Kategorie werden alle Artikeln von dem Typ auf 1 gesetzt

--- src/app/Einkauf/Category/Repository/DeleteSettingsRepository.php
+++ src/app/Einkauf/Category/Repository/DeleteSettingsRepository.php
@@ -9,7 +9,12 @@ class DeleteSettingsRepository
 
     public function deleteById($id): bool
     {
-        $db = new Database('EinkaufCat');
-        return $db->delete()->where("id", "=",":id")->args([':id' => $id])->run();
+        $db = new Database('EinkaufArticle');
+        if ($db->update(['cat_id'])->where("cat_id", "=", ":id")->args([':cat_id' => 1, ':id' => $id])->run()) {
+            $db = new Database('EinkaufCat');
+            return $db->delete()->where("id", "=", ":id")->args([':id' => $id])->run();
+        } else {
+            return false;
+        }
     }
 }
--- src/app/Einkauf/Shop/Repository/DeleteSettingsRepository.php
+++ src/app/Einkauf/Shop/Repository/DeleteSettingsRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Einkauf\Shop\Repository;
+
 use Foundation\Database\Database;
 
 class DeleteSettingsRepository
@@ -8,7 +9,12 @@ class DeleteSettingsRepository
 
     public function deleteById($id): bool
     {
-        $db = new Database('EinkaufShop');
-        return $db->delete()->where("id", "=",":id")->args([':id' => $id])->run();
+        $db = new Database('EinkaufArticle');
+        if ($db->update(['shop_id'])->where("shop_id", "=", ":id")->args([':shop_id' => 1, ':id' => $id])->run()) {
+            $db = new Database('EinkaufShop');
+            return $db->delete()->where("id", "=", ":id")->args([':id' => $id])->run();
+        } else {
+            return false;
+        }
     }
 }
